5b54c14074950f518a1be09a672a56223fd3594d
= <=>
2022-06-30 19:34:38 -0500

    Other class. EGP.

--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -1,3 +1,8 @@
+from dataclasses import Field
 from django.db import models
 
 # Create your models here.
+class Curso(models.Model):
+    
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField()
